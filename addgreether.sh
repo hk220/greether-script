a692e104c9ef4fd9b5832011ff22b1ebcf1cde0c
Kazuki <yukidama.k@gmail.com>
2015-12-03 14:29:42 -0500

    Add Feature: Automatically get the local ip address

--- addgreether.sh
+++ addgreether.sh
@@ -4,5 +4,7 @@ if [ $# -ne 3 ]; then
   exit 1;
 fi
 
-ip link add ${1} type gretap remote ${2} local ${3}
+local_ip=`ip addr show ${3} |sed -nEe 's/^[ \t]*inet[ \t]*([0-9.]+)\/.*$/\1/p'`
+
+ip link add ${1} type gretap remote ${2} local $local_ip
 ip link set ${1} up
